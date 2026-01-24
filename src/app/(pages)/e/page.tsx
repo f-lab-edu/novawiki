05894511f48ac6af716be8195b51053c853b786d
Leewonno <jsca5253@gmail.com>
2026-01-24 17:20:47 +0900

    [FEAT] 편집 페이지 입력 필드 추가

--- src/app/(pages)/e/page.tsx
+++ src/app/(pages)/e/page.tsx
@@ -3,6 +3,15 @@ export default function Edit() {
   return (
     <div>
       편집 페이지
+      {/* 입력 영역 */}
+      <textarea>
+
+      </textarea>
+
+      {/* 미리보기 */}
+      <div>
+
+      </div>
     </div>
   );
 }
