435387ff49ed4afceb39e02607b10ba916bfb5a6
Leewonno <jsca5253@gmail.com>
2026-02-12 12:30:57 +0900

    [UPDATE] 최근 문서 조회 api 수정

--- src/app/api/document/recent/route.ts
+++ src/app/api/document/recent/route.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@/lib/supabase/server";
+import { createAdminClient } from "@/lib/supabase/server";
 
 export async function GET() {
   // 작성자 정보가 필요 없으므로 adminClient 사용하지 않음
-  const supabase = await createClient();
+  const supabase = createAdminClient();
   const query = supabase
     .from("document")
     .select(`*`)
